0ee4ebd4720ace83e65ed5345d2fe044cb759a5a
Hilkiah Makemo <hilkiah.makemo@skywest.com>
2019-08-01 22:22:09 -0600

    Updated vendor-expose

--- src/datamodels/DarkAdmin.php
+++ src/datamodels/DarkAdmin.php
@@ -6,19 +6,19 @@ use SilverStripe\View\Requirements;
 class DarkAdmin extends ModelAdmin {
 	public function init() {
 		parent::init();
-		Requirements::javascript('darksite/javascript/darkAdmin.js');
+		Requirements::javascript('skywest/ss-darksite: darksite/javascript/darkAdmin.js');
 	}
-	
+
 	private static $managed_models = array(
 		'DarkSite',// => array('record_controller' => 'DarkAdmin_RecordController'),
 		'Partner'				// ! un-comment to edit partners list
 		//'DarkSite_Password'		// ! this line should only be un-commented out when you need to set or change the password!!!!!!
 	);
-	
+
 	private static $url_segment = 'darkAdmin';
 	private static $menu_title = 'Dark Site';
 	private static $set_page_length = 100;
-	
+
 	var $showImportForm = false;
 
 	function getEditForm($id = null, $fields = null){
